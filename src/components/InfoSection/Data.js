54286a930ccb1b056c476fabab00133e490bc96e
Fahad Kiani <fkiani@lt-c02f2311md6n.lan>
2022-01-17 03:13:31 -0500

    commit

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'One-stop-shop for your business',
+  headline: 'We help small businesses scale with web development',
+  description:
+    'Our digital web solutions enable restaurants to grow their presence and become competitive, automated and integrated ',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../../images/local.jpg'),
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Grow your revenue',
+  headline: 'Become profitable ',
+  description:
+    'Keep 100% of profit by offering your own online services',
+  buttonLabel: 'Learn more',
+  imgStart: true,
+  img: require('../../images/open.jpg'),
+  alt: 'Piggybank',
+  dark: false,
+  primary: false,
+  darkText: true
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join our Team',
+  headline: 'Creating an account is extremely easy',
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: 'Start Now',
+  imgStart: false,
+  img: require('../../images/svg-3.svg'),
+  alt: 'Papers',
+  dark: false,
+  primary: false,
+  darkText: true
+};
